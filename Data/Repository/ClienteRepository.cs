fe950cee2164fdcba1140a92360b26969cd3ee26
Isaias Pinto <isaiascxs10@gmail.com>
2023-07-13 12:50:28 -0300

    repository

---
+++ Data/Repository/ClienteRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Domain;
+using Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repository;
+
+public class ClienteRepository
+{
+    private readonly DatabaseContext _context;
+    public ClienteRepository(DatabaseContext context) 
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Cliente>> GetClientesAsync()
+    {
+        return await _context.Clientes.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Cliente> GetClienteAsync(int id)
+    {
+        return await _context.Clientes.FindAsync(id);
+    }
+
+}
