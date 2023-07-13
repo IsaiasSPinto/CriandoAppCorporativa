5724d3d768632464689e24d57f776693aeaa2394
Isaias Pinto <isaiascxs10@gmail.com>
2023-07-13 12:04:43 -0300

    docker compose

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,17 +1,39 @@
+using System;
+using Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("Conn");
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultorio Legal", Version = "v1" });
+});
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //using (var scope = app.Services.CreateScope())
+    //{
+    //    var dbContext = scope.ServiceProvider
+    //        .GetRequiredService<DatabaseContext>();
+
+    //    //dbContext.Database.Cre();
+    //    dbContext.Database.Migrate();
+    //}
     app.UseSwagger();
     app.UseSwaggerUI();
 }
---
+++ Data/Context/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
