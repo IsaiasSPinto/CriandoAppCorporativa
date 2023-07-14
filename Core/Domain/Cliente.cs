c1d497c4ad8c8503d93a2bddeb52c200f4b3d19d
Isaias Pinto <isaiascxs10@gmail.com>
2023-07-14 01:21:35 -0300

    Feat: nvos campos cliente

--- Core/Domain/Cliente.cs
+++ Core/Domain/Cliente.cs
@@ -10,7 +10,9 @@ namespace Core.Domain
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-
+        public char Sexo { get; set; }
+        public string Telefone { get; set; }
+        public string Documento { get; set; }
         public DateTime DataNascimento { get; set; }
     }
 }
